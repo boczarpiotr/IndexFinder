85815644e53b595f280c3aeaa48fd9499dc3c183
Piotr Boczar <boczarpiotr@gmail.com>
2020-03-22 14:12:42 +0100

    Added second method with only one loop

--- src/IndexFinder.java
+++ src/IndexFinder.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class IndexFinder {
-    
+
     public int[] twoSum(int[] tabOfInt, int target) {
 
         for (int i = 0; i < tabOfInt.length; i++) {
@@ -15,10 +15,29 @@ public class IndexFinder {
         return null;
     }
 
+    public int[] twoSumV2 (int[] tabOfInt, int target){
+        Map<Integer, Integer> missingValuesIndexes = new HashMap<>();
+
+        for (int i = 0; i <tabOfInt.length ; i++) {
+            if (missingValuesIndexes.containsKey(tabOfInt[i])){
+                return new int[] {i , missingValuesIndexes.get(tabOfInt[i])};
+            }
+            int missingValue = target - tabOfInt[i];
+            missingValuesIndexes.put(missingValue, i);
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         IndexFinder indexFinder = new IndexFinder();
         int[] tabOfInt = {1, 2, 3, 5};
+        // key 7 , v 0
+        // k 6 , v 1
+        // k 5, v 2
+        //
         int target = 8;
         System.out.println(Arrays.toString(indexFinder.twoSum(tabOfInt, target)));
+        System.out.println(Arrays.toString(indexFinder.twoSumV2(tabOfInt, target)));
     }
 }
